0c1d449ef242c4d2cb0d4233d358865654cf3e5f
Keith Suderman <suderman@cs.vassar.edu>
2016-07-07 16:49:41 -0400

    SVN Rank now downloaded by packages script.

--- packages.sh
+++ packages.sh
@@ -1,7 +1,7 @@
 # Since the .tgz packages are not kept in source control this script can be used
 # to download the packages.
 
-PACKAGE_LIST="lsd-latest brat"
+PACKAGE_LIST="lsd-latest brat svm_rank_linux64"
 
 if [ ! -d packages ] ; then
 	mkdir packages
